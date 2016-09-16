bff6f47307509f6f350655381f7ba76508c5ed4e
Adam Shaleen <adamshaleen@gmail.com>
2016-09-16 12:42:51 -0500

    Add files via upload

---
+++ MooseGreetings.java
@@ -0,0 +1,7 @@
+// idiot baby's first java program 
+
+public class MooseGreetings {
+    public static void main(String[] args) {
+        System.out.println("mooooooooooo"); //Prints moooooo to the console
+    }
+}
---
+++ bunchoDudez.java
@@ -0,0 +1,7 @@
+public class bunchoDudez {
+  public static void main(String[] args) {
+    for (int x = 0; x < 3; x++) {
+      System.out.println("BUNCHODUDES ARE FOR THE CHILDREN!");
+    }
+  }
+}
