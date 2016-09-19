91edcb5fff3bf00336bff8f4eb9eb926faf3da49
Adam Shaleen <adamshaleen@gmail.com>
2016-09-19 11:47:51 -0500

    Add files via upload

---
+++ EchoTestDrive.java
@@ -0,0 +1,28 @@
+public class EchoTestDrive {
+  public static void main(String[] args) {
+
+    Echo e1 = new Echo();
+    Echo e2 = new Echo();
+    int x = 0;
+
+    while(x < 4) {
+      e1.hello();
+      e1.count = e1.count + 1;
+      if(x == 3) {
+        e2.count = e2.count + 1;
+      }
+      if(x > 0) {
+        e2.count = e2.count + e1.count;
+      }
+      x = x + 1;
+    }
+    System.out.print(e2.count);
+  }
+}
+
+class Echo {
+  int count = 0;
+  void hello() {
+    System.out.println("hellooo...");
+  }
+}
