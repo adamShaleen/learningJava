739d790fa03d33adafbb32086d890624a54bd11b
Adam Shaleen <adamshaleen@gmail.com>
2016-10-24 08:19:56 -0500

    Update sandbox.java

--- sandbox.java
+++ sandbox.java
@@ -1,12 +1,14 @@
-import java.util.Arrays;
+//------------------Codewars 'Find the stray number'-----------------------------------
 
-class Solution {
-  static int stray(int[] numbers) {
-    Arrays.sort(numbers);
-    if (numbers[0] != numbers[1]) {
-      return numbers[0];
-    } else {
-      return numbers[numbers.length -1];
-    }
-  }
-}
+// import java.util.Arrays;
+
+// class Solution {
+//   static int stray(int[] numbers) {
+//     Arrays.sort(numbers);
+//     if (numbers[0] != numbers[1]) {
+//       return numbers[0];
+//     } else {
+//       return numbers[numbers.length -1];
+//     }
+//   }
+// }
