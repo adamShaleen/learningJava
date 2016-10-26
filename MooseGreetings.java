6ac6936bbc91e8727d3f5fbaa7a999287ce114bc
Adam Shaleen <adamshaleen@gmail.com>
2016-10-25 21:36:23 -0500

    Delete MooseGreetings.java

--- MooseGreetings.java
+++
@@ -1,7 +0,0 @@
-// idiot baby's first java program 
-
-public class MooseGreetings {
-    public static void main(String[] args) {
-        System.out.println("mooooooooooo"); //Prints moooooo to the console
-    }
-}
