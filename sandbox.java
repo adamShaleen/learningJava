4eb5ff0329967a4f6a7152640a9d5e5c6c5971d7
Adam Shaleen <adamshaleen@gmail.com>
2017-09-19 11:41:00 -0500

    Update sandbox.java

--- sandbox.java
+++ sandbox.java
@@ -1,3 +1,23 @@
+//-----------------Codewars Sum of odd numbers----------------------------------------
+
+// Given the triangle of consecutive odd numbers:
+
+//              1
+//           3     5
+//        7     9    11
+//    13    15    17    19
+// 21    23    25    27    29
+// ...
+// Calculate the row sums of this triangle from the row index (starting at index 1) e.g.:
+
+// rowSumOddNumbers(1); // 1
+// rowSumOddNumbers(2); // 3 + 5 = 8
+
+// public static int rowSumOddNumbers(int n) {
+    
+//     return n * n * n;  
+// }
+
 //-----------------Codewars Find the odd int------------------------------------------
 // first time using XOR operator
 
